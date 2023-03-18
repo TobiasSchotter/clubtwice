db9ccc2957d52a1f66d8d7e74d23e595a30e2689
1ric6 <117525177+1ric6@users.noreply.github.com>
2023-03-18 14:35:26 +0100

    Changes NavigationBar

--- lib/views/screens/page_switcher.dart
+++ lib/views/screens/page_switcher.dart
@@ -1,7 +1,5 @@
-import 'package:clubtwice/views/screens/cart_page.dart';
 import 'package:clubtwice/views/screens/message_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:clubtwice/constant/app_color.dart';
 import 'package:clubtwice/views/screens/feeds_page.dart';
 import 'package:clubtwice/views/screens/home_page.dart';
@@ -16,12 +14,6 @@ class PageSwitcher extends StatefulWidget {
 class _PageSwitcherState extends State<PageSwitcher> {
   int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,52 +24,37 @@ class _PageSwitcherState extends State<PageSwitcher> {
         MessagePage(),
         ProfilePage(),
       ][_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-            border:
-                Border(top: BorderSide(color: AppColor.primarySoft, width: 2))),
-        child: BottomNavigationBar(
-          onTap: _onItemTapped,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: [
-            (_selectedIndex == 0)
-                ? BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Home-active.svg'),
-                    label: '')
-                : BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Home.svg'), label: ''),
-            (_selectedIndex == 1)
-                ? BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Category-active.svg'),
-                    label: '')
-                : BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Category.svg'),
-                    label: ''),
-            (_selectedIndex == 2)
-                ? BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                        'assets/icons/Notification-active.svg'),
-                    label: '')
-                : BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Notification.svg'),
-                    label: ''),
-            (_selectedIndex == 3)
-                ? BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Message.svg'),
-                    label: '')
-                : BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Message.svg'),
-                    label: ''),
-            (_selectedIndex == 4)
-                ? BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Profile-active.svg'),
-                    label: '')
-                : BottomNavigationBarItem(
-                    icon: SvgPicture.asset('assets/icons/Profile.svg'),
-                    label: ''),
-          ],
-        ),
+      bottomNavigationBar: NavigationBar(
+        height: 60,
+        backgroundColor: AppColor.primarySoft,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        selectedIndex: _selectedIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Start',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: 'Suchen',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Verkaufen',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.mail_outline),
+            label: 'Nachrichten',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
       ),
     );
   }
