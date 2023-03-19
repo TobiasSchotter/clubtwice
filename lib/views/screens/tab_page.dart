9380cc8e353e10476303e248a3a737195dc02bb1
1ric6 <117525177+1ric6@users.noreply.github.com>
2023-03-19 10:08:16 +0100

    Bugfix

--- lib/views/screens/tab_page.dart
+++ lib/views/screens/tab_page.dart
@@ -1,14 +1,5 @@
-import 'dart:html';
-
-import 'package:clubtwice/views/screens/message_detail_page.dart';
-import 'package:clubtwice/views/screens/notification_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:clubtwice/constant/app_color.dart';
-import 'package:clubtwice/core/model/Message.dart';
-import 'package:clubtwice/core/services/MessageService.dart';
-import 'package:clubtwice/views/widgets/message_tile_widget.dart';
 
 class TabPage extends StatefulWidget {
   const TabPage({super.key});
@@ -18,8 +9,6 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
-  List<Message> listMessage = MessageService.messageData;
-
   @override
   Widget build(BuildContext context) => DefaultTabController(
         length: 2,
