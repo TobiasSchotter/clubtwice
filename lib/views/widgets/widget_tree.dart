fff356017bcd7ee3917013b2403a4a21fa353394
Tobias Schotter <tschotter98@gmx.de>
2023-03-26 21:15:41 +0200

    firebase 1

---
+++ lib/views/widgets/widget_tree.dart
@@ -0,0 +1,13 @@
+// import 'package:clubtwice/core/services/auth.dart';
+// import 'package:clubtwice/views/screens/login_page.dart';
+// import 'package:clubtwice/views/screens/register_page.dart';
+// import 'package:clubtwice/views/screens/home_page.dart';
+// import 'package:flutter/material.dart';
+
+
+// class WidgetTree extends StatefulWidget {
+//   const WidgetTree({super.key});
+
+//   @override
+//   State<WidgetTree> createState() => _WidgetTreeState();
+// }
