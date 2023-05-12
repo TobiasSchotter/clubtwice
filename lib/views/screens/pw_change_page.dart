6cca982f9e56edb17e148ae88173bf9badbc20de
1ric6 <117525177+1ric6@users.noreply.github.com>
2023-05-12 22:00:38 +0200

    pw_change_page pw change function

--- lib/views/screens/pw_change_page.dart
+++ lib/views/screens/pw_change_page.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:clubtwice/constant/app_color.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../constant/app_color.dart';
 
 class PWChangePage extends StatefulWidget {
-  const PWChangePage({super.key});
+  const PWChangePage({Key? key}) : super(key: key);
 
   @override
   State<PWChangePage> createState() => _PWChangePageState();
 }
 
 class _PWChangePageState extends State<PWChangePage> {
-  String? errorMessage = '';
-  bool isLogin = true;
+  final _auth = FirebaseAuth.instance;
+  final _oldPasswordController = TextEditingController();
+  final _newPasswordController = TextEditingController();
+  final _repeatPasswordController = TextEditingController();
+  String? _errorMessage = '';
+  bool _isLogin = true;
+  bool isObscured = true;
+  bool isObscured2 = true;
+  bool isObscured3 = true;
+
+  @override
+  void dispose() {
+    _oldPasswordController.dispose();
+    _newPasswordController.dispose();
+    _repeatPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +72,7 @@ class _PWChangePageState extends State<PWChangePage> {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(bottom: 32),
+            margin: const EdgeInsets.only(bottom: 16),
             child: Text(
               ' - Verwende mindestens 8 Zeichen\n - Wähle eine Kombination aus Buchstaben, Zahlen und Sonderzeichen',
               style: TextStyle(
@@ -64,11 +81,12 @@ class _PWChangePageState extends State<PWChangePage> {
                   height: 150 / 100),
             ),
           ),
-          const SizedBox(height: 16),
+          // const SizedBox(height: 16),
           // Password
           TextField(
+            controller: _oldPasswordController,
             autofocus: false,
-            obscureText: true,
+            obscureText: isObscured,
             decoration: InputDecoration(
               hintText: 'Altes Passwort eingeben',
               prefixIcon: Container(
@@ -90,16 +108,25 @@ class _PWChangePageState extends State<PWChangePage> {
               filled: true,
               //
               suffixIcon: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset('assets/icons/Hide.svg',
-                    color: AppColor.primary),
+                onPressed: () {
+                  setState(() {
+                    isObscured = !isObscured;
+                  });
+                },
+                icon: isObscured
+                    ? Icon(Icons.visibility_off,
+                        color: AppColor.primary.withOpacity(0.5), size: 20)
+                    : Icon(Icons.visibility,
+                        color: AppColor.primary.withOpacity(0.5), size: 20),
               ),
             ),
           ),
           const SizedBox(height: 16),
+
           TextField(
+            controller: _newPasswordController,
             autofocus: false,
-            obscureText: true,
+            obscureText: isObscured2,
             decoration: InputDecoration(
               hintText: 'Neues Passwort eingeben',
               prefixIcon: Container(
@@ -121,17 +148,25 @@ class _PWChangePageState extends State<PWChangePage> {
               filled: true,
               //
               suffixIcon: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset('assets/icons/Hide.svg',
-                    color: AppColor.primary),
+                onPressed: () {
+                  setState(() {
+                    isObscured2 = !isObscured2;
+                  });
+                },
+                icon: isObscured2
+                    ? Icon(Icons.visibility_off,
+                        color: AppColor.primary.withOpacity(0.5), size: 20)
+                    : Icon(Icons.visibility,
+                        color: AppColor.primary.withOpacity(0.5), size: 20),
               ),
             ),
           ),
           const SizedBox(height: 16),
           // Repeat Password
           TextField(
+            controller: _repeatPasswordController,
             autofocus: false,
-            obscureText: true,
+            obscureText: isObscured3,
             decoration: InputDecoration(
               hintText: 'Neues Passwort wiederholen',
               prefixIcon: Container(
@@ -153,23 +188,22 @@ class _PWChangePageState extends State<PWChangePage> {
               filled: true,
               //
               suffixIcon: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset('assets/icons/Hide.svg',
-                    color: AppColor.primary),
+                onPressed: () {
+                  setState(() {
+                    isObscured3 = !isObscured3;
+                  });
+                },
+                icon: isObscured3
+                    ? Icon(Icons.visibility_off,
+                        color: AppColor.primary.withOpacity(0.5), size: 20)
+                    : Icon(Icons.visibility,
+                        color: AppColor.primary.withOpacity(0.5), size: 20),
               ),
             ),
           ),
           const SizedBox(height: 24),
-
           ElevatedButton(
-            onPressed: () {
-              //Nav needs to be removed after firebase integration
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Passwort erfolgreich geändert'),
-                ),
-              );
-            },
+            onPressed: _changePassword,
             style: ElevatedButton.styleFrom(
               padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
               backgroundColor: AppColor.primary,
@@ -187,8 +221,87 @@ class _PWChangePageState extends State<PWChangePage> {
                   fontFamily: 'poppins'),
             ),
           ),
+          if (_errorMessage != null && _errorMessage!.isNotEmpty) ...[
+            const SizedBox(height: 16),
+            Text(
+              _errorMessage!,
+              style: const TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ],
       ),
     );
+  }
+
+  Future<void> _changePassword() async {
+    setState(() {
+      _errorMessage = '';
+    });
+
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final oldPassword = _oldPasswordController.text.trim();
+    final newPassword = _newPasswordController.text.trim();
+    final repeatPassword = _repeatPasswordController.text.trim();
+
+    if (oldPassword.isEmpty || newPassword.isEmpty || repeatPassword.isEmpty) {
+      setState(() {
+        _errorMessage = 'Bitte alle Felder ausfüllen';
+      });
+      return;
+    }
+
+    final credential = EmailAuthProvider.credential(
+        email: currentUser!.email!, password: oldPassword);
+
+    try {
+      await currentUser.reauthenticateWithCredential(credential);
+
+      if (newPassword.length < 8 ||
+          !newPassword.contains(RegExp(r'[0-9]')) ||
+          !newPassword.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+        setState(() {
+          _errorMessage =
+              'Das neue Passwort muss mindestens 8 Zeichen lang sein und mindestens eine Zahl und ein Sonderzeichen enthalten.';
+        });
+        return;
+      }
+
+      if (newPassword != repeatPassword) {
+        setState(() {
+          _errorMessage =
+              'Die wiederholte Eingabe des neuen Passworts stimmt nicht überein.';
+        });
+        return;
+      }
+
+      await currentUser.updatePassword(newPassword);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Passwort erfolgreich geändert.'),
+        ),
+      );
+
+      Navigator.of(context).pop();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'wrong-password') {
+        setState(() {
+          _errorMessage = 'Das alte Passwort ist falsch.';
+        });
+      } else {
+        setState(() {
+          _errorMessage =
+              'Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage =
+            'Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.';
+      });
+    }
   }
 }
