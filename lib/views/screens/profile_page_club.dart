dbf9c9d5e74d3b2790242bfde2f2604b228aa8dc
1ric6 <117525177+1ric6@users.noreply.github.com>
2023-05-20 22:42:02 +0200

    get values

--- lib/views/screens/profile_page_club.dart
+++ lib/views/screens/profile_page_club.dart
@@ -1,3 +1,4 @@
+import 'package:clubtwice/views/screens/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:clubtwice/constant/app_color.dart';
@@ -6,7 +7,7 @@ import '../../constant/app_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePageClub extends StatefulWidget {
-  const ProfilePageClub({super.key});
+  const ProfilePageClub({Key? key}) : super(key: key);
 
   @override
   State<ProfilePageClub> createState() => _ProfilePageClubState();
@@ -16,8 +17,30 @@ class _ProfilePageClubState extends State<ProfilePageClub> {
   String verein = '';
   String sportart = '';
 
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      String userId = user.uid;
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
+          .instance
+          .collection('users')
+          .doc(userId)
+          .get();
+      Map<String, dynamic> userData = snapshot.data() ?? {};
+      setState(() {
+        verein = userData['club'] ?? '';
+        sportart = userData['sport'] ?? '';
+      });
+    }
+  }
+
   Future<void> saveChanges() async {
-    // Beispielaufruf der Funktion
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       String userId = user.uid;
@@ -41,14 +64,25 @@ class _ProfilePageClubState extends State<ProfilePageClub> {
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
-        title: const Text('Verein und Sportart anpassen',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontWeight: FontWeight.w600)),
+        title: const Text(
+          'Verein und Sportart anpassen',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
+          onPressed: () async {
+            bool shouldRefresh =
+                await Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ProfilePage(),
+            ));
+
+            if (shouldRefresh == true) {
+              // Führe die Aktualisierungslogik aus
+              // ...
+            }
           },
           icon: const Icon(Icons.arrow_back_outlined),
           color: Colors.black,
@@ -60,64 +94,64 @@ class _ProfilePageClubState extends State<ProfilePageClub> {
         padding: const EdgeInsets.symmetric(horizontal: 24),
         physics: const BouncingScrollPhysics(),
         children: [
-          // Section 1 - Header
-
           Container(
             margin: const EdgeInsets.only(bottom: 0),
             child: Text(
               'Deine Startseite wird dir dementsprechend angezeigt. Bei Verkäufen wird dies bereits vorausgefüllt sein.\nDu kannst diese jederzeit ändern.',
               style: TextStyle(
-                  color: AppColor.secondary.withOpacity(0.7),
-                  fontSize: 12,
-                  height: 150 / 100),
+                color: AppColor.secondary.withOpacity(0.7),
+                fontSize: 12,
+                height: 150 / 100,
+              ),
             ),
           ),
-          // Section 2  - Form
-
           Container(
             height: 16,
           ),
           DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: 'Verein auswählen',
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['SG Quelle', 'SGV Nürnberg Fürth']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  verein = newValue!;
-                });
-              }),
+            decoration: const InputDecoration(
+              labelText: 'Verein auswählen',
+              border: OutlineInputBorder(),
+            ),
+            value: verein.isNotEmpty ? verein : null, // Set the initial value
+            items: <String>['SG Quelle', 'SGV Nürnberg Fürth']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                verein = newValue!;
+              });
+            },
+          ),
           Container(
             height: 16,
           ),
           DropdownButtonFormField<String>(
-              focusColor: AppColor.primarySoft,
-              decoration: const InputDecoration(
-                labelText: 'Sportart auswählen',
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['Fußball', 'Basketball']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  sportart = newValue!;
-                });
-              }),
-
+            focusColor: AppColor.primarySoft,
+            decoration: const InputDecoration(
+              labelText: 'Sportart auswählen',
+              border: OutlineInputBorder(),
+            ),
+            value:
+                sportart.isNotEmpty ? sportart : null, // Set the initial value
+            items: <String>['Fußball', 'Basketball']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                sportart = newValue!;
+              });
+            },
+          ),
           const SizedBox(height: 16),
-
           CustomButton(
             buttonText: 'Speichern',
             onPressed: () {
--- lib/views/widgets/profile_tile_widget.dart
+++ lib/views/widgets/profile_tile_widget.dart
@@ -1,121 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyProfileWidget extends StatelessWidget {
   const MyProfileWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //height: 241,
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/background.jpg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: [
-          // Profile Picture
-          Container(
-            width: 96,
-            height: 96,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.grey,
-              image: DecorationImage(
-                image: AssetImage('assets/images/pp.jpg'),
-                fit: BoxFit.cover,
-              ),
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return Container(); // Handle when user is not logged in
+    }
+
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      future:
+          FirebaseFirestore.instance.collection('users').doc(user.uid).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // Handle while data is being fetched
+        }
+
+        final userData = snapshot.data?.data();
+        if (userData == null) {
+          return Container(); // Handle when user data is not available
+        }
+
+        final firstname = userData['first Name'] ?? '';
+        final lastname = userData['last Name'] ?? '';
+        final club = userData['club'] ?? '';
+        final sport = userData['sport'] ?? '';
+
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
             ),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                iconSize: 15,
-                icon: Icon(Icons.camera_alt_rounded),
-                color: Colors.white,
-                onPressed: () async {
-                  final pickedFile = await showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return SafeArea(
-                        child: Wrap(
-                          children: [
-                            ListTile(
-                              leading: Icon(Icons.photo_library),
-                              title: Text('Galerie'),
-                              onTap: () async {
-                                final picker = ImagePicker();
-                                final pickedImage = await picker.pickImage(
-                                  source: ImageSource.gallery,
-                                  maxWidth: 400,
-                                );
-                                Navigator.of(context).pop(pickedImage);
-                              },
+          ),
+          child: Column(
+            children: [
+              // Profile Picture
+              Container(
+                width: 96,
+                height: 96,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/pp.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    iconSize: 15,
+                    icon: Icon(Icons.camera_alt_rounded),
+                    color: Colors.white,
+                    onPressed: () async {
+                      // Image picking logic
+                      final pickedFile = await showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SafeArea(
+                            child: Wrap(
+                              children: [
+                                ListTile(
+                                  leading: Icon(Icons.photo_library),
+                                  title: Text('Galerie'),
+                                  onTap: () async {
+                                    final picker = ImagePicker();
+                                    final pickedImage = await picker.pickImage(
+                                      source: ImageSource.gallery,
+                                      maxWidth: 400,
+                                    );
+                                    Navigator.of(context).pop(pickedImage);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.camera_alt),
+                                  title: Text('Kamera'),
+                                  onTap: () async {
+                                    final picker = ImagePicker();
+                                    final pickedImage = await picker.pickImage(
+                                      source: ImageSource.camera,
+                                      maxWidth: 400,
+                                    );
+                                    Navigator.of(context).pop(pickedImage);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.delete),
+                                  title: Text('Bild löschen'),
+                                  onTap: () {
+                                    Navigator.of(context).pop(null);
+                                  },
+                                ),
+                              ],
                             ),
-                            ListTile(
-                              leading: Icon(Icons.camera_alt),
-                              title: Text('Kamera'),
-                              onTap: () async {
-                                final picker = ImagePicker();
-                                final pickedImage = await picker.pickImage(
-                                  source: ImageSource.camera,
-                                  maxWidth: 400,
-                                );
-                                Navigator.of(context).pop(pickedImage);
-                              },
-                            ),
-                            ListTile(
-                              leading: Icon(Icons.delete),
-                              title: Text('Bild löschen'),
-                              onTap: () {
-                                Navigator.of(context).pop(null);
-                              },
-                            ),
-                          ],
-                        ),
+                          );
+                        },
                       );
+                      if (pickedFile != null) {
+                        // Hier kannst du den Code zum Hochladen des Bildes einfügen
+                        // Beispiel für die Verwendung des Bildes:
+                      }
                     },
-                  );
-                  if (pickedFile != null) {
-                    // Hier kannst du den Code zum Hochladen des Bildes einfügen
-                    // Beispiel für die Verwendung des Bildes:
-                  }
-                },
+                  ),
+                ),
               ),
-            ),
+              // Fullname
+              Container(
+                margin: const EdgeInsets.only(bottom: 4, top: 14),
+                child: Text(
+                  '$firstname' + ' $lastname',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              // Username
+              Text(
+                'UserName',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.6),
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                '$club',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.6),
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                '$sport',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.6),
+                  fontSize: 14,
+                ),
+              ),
+            ],
           ),
-          // Fullname
-          Container(
-            margin: const EdgeInsets.only(bottom: 4, top: 14),
-            child: const Text(
-              'Riccardo Bald',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16),
-            ),
-          ),
-          // Username
-          Text(
-            '@RicBal',
-            style:
-                TextStyle(color: Colors.white.withOpacity(0.6), fontSize: 14),
-          ),
-          Text(
-            'SG Quelle Fürth',
-            style:
-                TextStyle(color: Colors.white.withOpacity(0.6), fontSize: 14),
-          ),
-          Text(
-            'Fußball',
-            style:
-                TextStyle(color: Colors.white.withOpacity(0.6), fontSize: 14),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
