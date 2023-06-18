bb0d1043c01eb2c28991950f00eec1527fb629e0
1ric6 <117525177+1ric6@users.noreply.github.com>
2023-06-18 17:51:03 +0200

    image_picker_widget gui

--- lib/views/widgets/image_picker_widget.dart
+++ lib/views/widgets/image_picker_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:clubtwice/constant/app_button.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -15,10 +16,9 @@ class ImagePickerWidget extends StatefulWidget {
 class _ImagePickerWidgetState extends State<ImagePickerWidget> {
   List<XFile> selectedImages = [];
 
-  Future<void> pickImages() async {
+  Future<void> pickImages(ImageSource source) async {
     if (selectedImages.length < 5) {
-      final XFile? image =
-          await ImagePicker().pickImage(source: ImageSource.gallery);
+      final XFile? image = await ImagePicker().pickImage(source: source);
 
       if (image != null) {
         setState(() {
@@ -26,6 +26,31 @@ class _ImagePickerWidgetState extends State<ImagePickerWidget> {
         });
         widget.onImagesSelected(selectedImages);
       }
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Limit erreicht'),
+            content:
+                const Text('Es können maximal 5 Bilder hochgeladen werden.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void _removeImage(int index) {
+    if (selectedImages.isNotEmpty) {
+      setState(() {
+        selectedImages.removeAt(index);
+      });
     }
   }
 
@@ -34,26 +59,75 @@ class _ImagePickerWidgetState extends State<ImagePickerWidget> {
     return Column(
       children: [
         SizedBox(
-          height: 200,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: selectedImages.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Image.file(
-                  File(selectedImages[index].path),
-                  fit: BoxFit.cover,
+          height: 150,
+          child: selectedImages.isNotEmpty
+              ? ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: selectedImages.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Stack(
+                        children: [
+                          Image.file(
+                            File(selectedImages[index].path),
+                            fit: BoxFit.cover,
+                            width: 100,
+                            height: 150,
+                          ),
+                          Positioned(
+                            top: 4.0,
+                            right: 4.0,
+                            child: GestureDetector(
+                              onTap: () => _removeImage(index),
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.red,
+                                ),
+                                padding: const EdgeInsets.all(4.0),
+                                child: const Icon(
+                                  Icons.close,
+                                  size: 16.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )
+              : Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/background.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              );
-            },
-          ),
         ),
-        SizedBox(height: 16),
-        ElevatedButton(
-          onPressed: pickImages,
-          child: Text('Bilder auswählen'),
-        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: CustomButton(
+                onPressed: () => pickImages(ImageSource.gallery),
+                buttonText: 'Galerie',
+              ),
+            ),
+            const SizedBox(
+                width:
+                    16), // Fügt einen horizontalen Abstand zwischen den Buttons hinzu
+            Expanded(
+              child: CustomButton(
+                onPressed: () => pickImages(ImageSource.camera),
+                buttonText: 'Kamera',
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
